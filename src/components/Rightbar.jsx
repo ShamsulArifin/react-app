ccd23165bea973cbde8a9632739c565a977470be
Shamsul Arifin <arifinbd6@gmail.com>
2022-06-04 23:51:35 +0600

    Changes

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const Rightbar = () => {
+  return (
+    <Box bgcolor="#4C3575" flex={2} p={2} sx={{ display: { xs: "none", sm: "block"}}}>
+        Rightbar
+    </Box>
+  )
+}
+
+export default Rightbar
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+	palette:{
+		primary: {
+			main: "#371B58",
+			light: "#ffdf00"
+		},
+		secondary:{
+			main: "#C0C0C0",
+			light: "#D3D3D3"
+		}
+	}
+})
